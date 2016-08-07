ac8e87c76104e52566dfe7b2ec5eab91f9757188
Madelyn <mnelso12@nd.edu>
2016-08-07 01:27:09 -0400

    upload.html stuff

---
+++ scripts/upload.js
@@ -0,0 +1,33 @@
+var file = $("#speech_file");
+console.log(file);
+
+$("#submit").click(function() {
+	console.log("pressed submit");
+
+
+});
+
+$(document).ready(function(){
+
+	console.log(httpGetAsync('http://facebook.com'));
+
+});
+
+function httpGetAsync(theUrl, callback)
+{
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() { 
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			callback(xmlHttp.responseText);
+	}
+	xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+	xmlHttp.send(null);
+}
+
+
+function httpGet(theUrl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+	xmlHttp.send( null );
+	return xmlHttp.responseText;
+}
